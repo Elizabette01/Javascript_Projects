5198ff39a1c2e94b3e6c62eaf141ecb117718b52
Elizabette N <ndukaelizabeth26@yahoo.com>
2024-08-06 15:10:14 +0100

    to-do list app completed

--- to_do_list_app/main.js
+++ to_do_list_app/main.js
@@ -1,7 +1,10 @@
-const inputDate = document.getElementById("date");
+ const inputDate = document.getElementById("date");
 const listInput = document.getElementById("listInput");
 const inputBtn = document.getElementById("inputBtn");
 const toDos = document.getElementById("toDos");
+const progressBar = document.getElementById("progress-bar");
+const progressNum = document.getElementById("progress-num");
+const praise = document.getElementById("praise");
 
 // Dynamic date function
 const todayDate = new Date();
@@ -25,6 +28,12 @@ inputDate.innerText = `${todayWeekDay}, ${todayDay} / ${todayMonth} / ${todayYea
 
 let toDoList = [];
 
+inputBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  addToList();
+});
+
 const addToList = () => {
   const text =  listInput.value.trim();
 
@@ -33,6 +42,7 @@ const addToList = () => {
     listInput.value = "";
 
     updateList();
+    statusUpdate();
   }
 }
 
@@ -44,20 +54,76 @@ const updateList = () => {
 
     listItem.innerHTML = `
       <input type="checkbox" name="" class="checkbox" ${task.completed ? "checked" : ""}>
-      <label for="">${toDoList.text}</label>
+      <label for="" class="${task.completed ? "completed" : ""}">${task.text}</label>
       <div class="todo-btns">
         <button onclick="editList(${index})"><i class="bi bi-pencil-square"></i></button>
-        <button onclick="deleteList(${index})><i class="bi bi-trash3"></i></button>
+        <button onclick="deleteList(${index})"><i class="bi bi-trash3"></i></button>
       </div>
     `;
 
-    listItem,addEventListener("change", () => toggleTakComplete(index));
+    listItem.addEventListener("change", () => toggleTaskComplete(index));
+
     toDos.append(listItem)
   })
 }
 
-inputBtn.addEventListener("click", (e) => {
-   e.preventDefault();
+// Function to toggle task completed or not
+const toggleTaskComplete = (index) => {
+  toDoList[index].completed = !toDoList[index].completed
 
-   addToList();
-});
+  console.log(toDoList);
+
+  statusUpdate();
+}
+
+// function to delete task
+
+const deleteList = (index) => {
+  toDoList.splice(index,1);
+
+  updateList();
+
+  statusUpdate();
+}
+
+// Function to edit task
+
+const editList = (index) => {
+  listInput.value = toDoList[index].text;
+
+  toDoList.splice(index,1);
+  updateList();
+
+  statusUpdate();
+}
+
+// Implementing progress bar
+
+const statusUpdate = ()=>{
+  const completedTasks = toDoList.filter(list => list.completed).length;
+  const totalTasks = toDoList.length;
+  const progress = (completedTasks / totalTasks) * 100;
+
+  progressBar.style.width = `${progress}%`;
+
+  progressNum.innerText = `${completedTasks} / ${totalTasks}`
+
+  if(progress === 0){
+    praise.innerText = "Let's get some task done"
+  }
+  else if(progress > 0 && progress <= 25){
+    praise.innerText = "Good job getting started"
+  }
+  else if(progress <= 49){
+    praise.innerText = "Keep up the momentum"
+  }
+  else if(progress === 50){
+    praise.innerText = "Half way through "
+  }
+  else if(progress <= 75){
+    praise.innerText = "We are almost there "
+  }
+  else if(progress === 100){
+    praise.innerText = "Congratulations!!! You did it"
+  }
+}
